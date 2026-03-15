3ea60ac0582a486a88b6c21a1dc2755b034fe08e
Anshika <anshikapriya12@gmail.com>
2026-03-15 20:37:22 +0530

    feat: Set up initial Django REST API backend project with accounts, students, and attendance applications.

---
+++ backend/students/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll_no = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='students/')
+
+    def __str__(self):
+        return self.name
